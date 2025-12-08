c1586029ee7898d31fb6661e5dc0d41153b6bb9a
codex-xx <vesterlaurel@gmail.com>
2025-12-08 23:20:20 +0800

    Add gmail verifications

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/CHAKANOKS-1';
+    public string $baseURL = 'http://localhost/CHAKANOKS';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
